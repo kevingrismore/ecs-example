a488b9c60a8bfe7a56c5c88bc80f5ff92877c16b
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2023-11-28 13:18:47 -0500

    add github action

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM prefecthq/prefect:2.14.0-python3.11
+FROM prefecthq/prefect:2.14.6-python3.11
 COPY . /opt/prefect/ecs-example/
 WORKDIR /opt/prefect/ecs-example/
--- flow.py
+++ flow.py
@@ -1,8 +1,24 @@
 from prefect import flow
+from prefect.deployments.runner import DeploymentImage
+from git import Repo
+
 
 @flow(log_prints=True)
 def hello():
     print("Hello Prefect!")
 
-if __name__=="__main__":
-    hello()
+
+if __name__ == "__main__":
+    repo = Repo()
+    commit_hash = repo.git.rev_parse("HEAD")
+
+    hello.deploy(
+        name="ecs-hello-example",
+        work_pool_name="my-ecs-pool",
+        image=DeploymentImage(
+            name="455346737763.dkr.ecr.us-east-2.amazonaws.com/kevin-test-repo",
+            tag=commit_hash,
+            dockerfile="Dockerfile",
+            platform="linux/amd64",
+        ),
+    )
