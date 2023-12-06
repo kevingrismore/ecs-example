29a06d116f2ec826b373d7c1511fdebd254c5513
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2023-12-06 15:37:56 -0500

    try to fail refresh

--- retry_flow.py
+++ retry_flow.py
@@ -1,21 +1,20 @@
-from prefect import flow, task
+from prefect import flow
+
+from google.auth.transport.requests import Request
+from google.auth import exceptions
+from google.auth.credentials import Credentials
 
 
-@flow(retries=1)
-def testing_retries():
-    successful_task()
-    failing_task()
+@flow()
+def main():
+    credentials = Credentials.from_service_account_file("YOUR_JSON_KEY_FILE.json")
 
-
-@task
-def successful_task():
-    return True
-
-
-@task
-def failing_task():
-    raise Exception("bad")
+    try:
+        # Attempt to refresh the token
+        credentials.refresh(Request())
+    except exceptions.RefreshError as e:
+        print(f"Error refreshing the token: {e}")
 
 
 if __name__ == "__main__":
-    testing_retries()
+    main()
